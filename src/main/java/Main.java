67fea175eab44c4d29f1383c12528263d9d6a383
Alexey <alexxx1809@gmail.com>
2020-09-01 09:22:53 +0500

    Начал писать код

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,38 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static SimpleDateFormat formatter = new SimpleDateFormat(
+            "yyyyMMdd_HHmmss");
 
+    public void robo() throws AWTException, IOException {
 
-        //TODO: 20200831_175809
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd " +
-                "HH:mm:ss");
+        Calendar now = Calendar.getInstance();
+        Robot robot = new Robot();
+        BufferedImage image =
+                robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        ImageIO.write(image, "JPG", new File("C:/Users/alexx/Pictures" +
+                "/Saved Pictures" + formatter.format(now.getTime()) + ".jpg"));
+        System.out.println(image.getWidth() + "x" + image.getHeight());
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
         Date date = new Date();
         System.out.println(formatter.format(date));
 
-        //TODO: получить скриншот экрана в переменную image
-        BufferedImage image = null;
-        System.out.println(image.getWidth() + "x" + image.getHeight());
+        Main s2i = new Main();
+        while (true) {
+            s2i.robo();
+            Thread.sleep(10000);
+        }
     }
 }
